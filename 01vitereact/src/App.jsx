c423e50309deaa473ab7440cabafaadfd7efd3ce
Muz1058 <muzaffarali3633@gmail.com>
2025-06-05 23:12:03 +0500

    Update App.jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
 
+  const variable="i am variable"
+
   return (
      <>
     <h1>Conventions</h1>
@@ -19,6 +21,11 @@ function App() {
     <p>npx create-react-app add javaScript to the files via dependencies  "react-scripts": "5.0.1"</p>
     <p>npm create vite@latest add javaScript to the file via  script tag with src</p>
     <p>in "npm create vite@latest" it is essential tha the javaScript file extension will be .jsx</p>
+    <h2>Variable in JSX</h2>
+    <p>in react variable are inserted inside CURLY BRaces without "$" sign</p>
+    <p>These varible are inserted as evalueated expression form mean only final outcome. You have to perform operation or condition before</p>
+    <p>This is because react convert code to object and then renders it. as you can add mathematical expressions to objects so we ad evalueated final resultant variable</p>
+    <p>{variable}</p>
     </>
   )
 }
