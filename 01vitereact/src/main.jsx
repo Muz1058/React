e40ac3045ba3cd2ad2ad1b0eb4fcde7e5880263f
Muz1058 <muzaffarali3633@gmail.com>
2025-06-05 23:02:51 +0500

    Update main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from 'react';
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  );
+}
+
+// const ReactElement={
+//     type:'a',
+//     props:{
+//         href:"https://google.com",
+//         target:"_blank"
+//     },
+//     children:"Click to visit google"
+// }
+
+// const anotherElement=(
+//   <a href="https://google.com" target="_blank">Visit Google by anotherElement</a>
+// )
+
+// eslint-disable-next-line no-undef
+const ReactElement= React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  'Visit Google by reactElement'
+
 )
+
+createRoot(document.getElementById("root")).render(
+
+    ReactElement
+
+);
