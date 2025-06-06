7d514950d1b5cf8170477a155300dc8b4973fb06
Muz1058 <muzaffarali3633@gmail.com>
2025-06-06 12:40:09 +0500

    Update App.jsx

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+
+import Card from './components/Card'
 import './App.css'
 
 function App() {
 
-
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-10'>Tailwind test</h1>
+
+     <Card userName="Tom" btnText="Click me"/>
+     <Card userName="Delba" btnText="Visit me"/>
+     
 
 
      
