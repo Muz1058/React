b8d4a6748799b37277c0041672144f791c96e34b
Muz1058 <muzaffarali3633@gmail.com>
2025-06-10 22:38:10 +0500

    Create TodoContext.js

---
+++ 10Todo_context_local/src/contexts/TodoContext.js
@@ -0,0 +1,20 @@
+import React,{createContext,useContext} from "react";
+
+export const TodoContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:" Todo msg",
+            completed: false
+        }
+    ]
+});
+
+
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+
+export const TodoProvider=TodoContext.Provider
