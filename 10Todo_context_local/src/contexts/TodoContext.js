c9a5b24842bf2bf3d1b089e394c3122617de969a
Muz1058 <muzaffarali3633@gmail.com>
2025-06-12 17:13:02 +0500

    Update TodoContext.js

--- 10Todo_context_local/src/contexts/TodoContext.js
+++ 10Todo_context_local/src/contexts/TodoContext.js
@@ -1,20 +1,22 @@
-import React,{createContext,useContext} from "react";
+import {createContext, useContext} from "react"
 
-export const TodoContext=createContext({
-    todos:[
+export const TodoContext = createContext({
+    todos: [
         {
-            id:1,
-            todo:" Todo msg",
-            completed: false
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
         }
-    ]
-});
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 
-
-export const useTodo = ()=>{
+export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-
-export const TodoProvider=TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
