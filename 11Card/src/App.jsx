924fff8fc4c6d94ef291deedfae9425afadddf82
Muz1058 <muzaffarali3633@gmail.com>
2025-06-13 23:22:50 +0500

    Create App.jsx

---
+++ 11Card/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
