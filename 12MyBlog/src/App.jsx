323de90cdd758a0d4f14bc74aac9340bd0f44470
Muz1058 <muzaffarali3633@gmail.com>
2025-07-25 03:43:20 -0700

    Update App.jsx

--- 12MyBlog/src/App.jsx
+++ 12MyBlog/src/App.jsx
@@ -3,8 +3,10 @@ import {useDispatch} from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
 import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components/index'
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const  [loading,setLoading]=useState(true)
   const dispatch=useDispatch()
 
@@ -21,7 +23,16 @@ function App() {
 
   },[])
 
-  
+  return !loading?(
+    <div className='bg-gray-400 h-screen w-screen'>
+    <Header/>
+    <main>
+      {/* <Outlet/> */}
+    </main>
+    <Footer/>
+    </div>
+    
+  ):null
 }
 
 export default App
