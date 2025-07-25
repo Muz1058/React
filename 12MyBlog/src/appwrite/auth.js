6e6aadd049ec9ed7a439b7e74bfdd485989b04c9
Muz1058 <muzaffarali3633@gmail.com>
2025-07-25 03:43:23 -0700

    Update auth.js

--- 12MyBlog/src/appwrite/auth.js
+++ 12MyBlog/src/appwrite/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import conf from "../conf/conf.js";
 
 import {Client,Account,ID} from 'appwrite'
@@ -46,6 +47,7 @@ export class AuthService{
             throw error;
         }
         
+        // eslint-disable-next-line no-unreachable
         return null;
     }
 
